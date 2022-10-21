55a0e246b4b54234e0221b4d0cecc8530e96a6e5
lenanbagus <lenanbagus@outlook.com>
2022-10-21 16:58:11 +0700

    edit bg

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,31 +8,33 @@
   <link rel="stylesheet" href="mystyle.css">
   <title>Document</title>
 </head>
+
 <body>
-  
 
-<div class="form-structor">
-	<div class="signup">
-		<h2 class="form-title" id="signup"><span>or</span>Daftar</h2>
-		<div class="form-holder">
-			<input type="text" class="input" placeholder="Nama" />
-			<input type="email" class="input" placeholder="Email" />
-			<input type="password" class="input" placeholder="Password" />
-		</div>
-		<button class="submit-btn">Sign up</button>
-	</div>
-	<div class="login slide-up">
-		<div class="center">
-			<h2 class="form-title" id="login"><span>or</span>Masuk</h2>
-			<div class="form-holder">
-				<input type="email" class="input" placeholder="Email" />
-				<input type="password" class="input" placeholder="Password" />
-			</div>
-			<button class="submit-btn">Log in</button>
-		</div>
-	</div>
-</div>
 
-<script src="myjs.js"></script>
+  <div class="form-structor">
+    <div class="signup">
+      <h2 class="form-title" id="signup">Daftar</h2>
+      <div class="form-holder">
+        <input type="text" class="input" placeholder="Nama" />
+        <input type="email" class="input" placeholder="Email" />
+        <input type="password" class="input" placeholder="Password" />
+      </div>
+      <button class="submit-btn">Sign up</button>
+    </div>
+    <div class="login slide-up">
+      <div class="center">
+        <h2 class="form-title" id="login">Masuk</h2>
+        <div class="form-holder">
+          <input type="email" class="input" placeholder="Email" />
+          <input type="password" class="input" placeholder="Password" />
+        </div>
+        <button class="submit-btn">Log in</button>
+      </div>
+    </div>
+  </div>
+
+  <script src="myjs.js"></script>
 </body>
+
 </html>
